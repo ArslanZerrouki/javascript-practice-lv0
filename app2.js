3225fb9cf26721bcdfb650583fbc95875dca36ab
ArslanZerrouki <113772680+ArslanZerrouki@users.noreply.github.com>
2022-10-14 17:17:42 +0100

    Finished

---
+++ app2.js
@@ -0,0 +1,7 @@
+function Fullname() {
+    let Firstname
+    let Familyname
+    Firstname =prompt("What is your First name")
+    Familyname =prompt("What is your Family name")
+    alert ("Your fullname is "+ Firstname + " " + Familyname)    
+}
